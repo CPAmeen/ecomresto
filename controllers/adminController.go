b46a6cbb09647b1d46a8573ad4f8494d1658e8a9
CPAmeen <ameenmuhammed022@gmail.com>
2024-10-16 17:18:10 +0530

    validate

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -65,11 +65,11 @@ func Login(c *gin.Context) {
 	c.SetSameSite(http.SameSiteLaxMode)
 	c.SetCookie("userAuthorization", tokenString, 3600*24*30, "", "", false, true)
 	c.JSON(http.StatusOK, gin.H{
-		"token created": tokenString,
+		"token": tokenString,
 	})
 }
 func Validate(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "I am logged in.",
+		"message": "I am loggged in.",
 	})
 }
